c4f2e61d747a397a58a460cbf23a7aa9943688ee
cognac <femt456@gmail.com>
2018-05-05 05:53:42 +0000

    task bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
+Task.create(content: "from seed1", status: "from seed2")
